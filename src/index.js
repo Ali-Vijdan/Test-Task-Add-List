4123762eb6185d600a7136f3f1d45768af0ee291
Vijdan Ali <alivijdan55@gmail.com>
2021-08-31 07:10:48 -0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './layout/TestTask/TestTask.js';
+
+ReactDOM.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById("root")
+);
